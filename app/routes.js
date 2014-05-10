bf32b3a732c288dd881be8d9eff808d20917aa56
Anuj Thapliyal <athapliyal@yp.com>
2014-05-09 17:42:04 -0700

    Setting up static file serving

--- app/routes.js
+++ app/routes.js
@@ -5,9 +5,7 @@ module.exports = function(app, passport) {
 	var express = require('express');
 	var path = require('path');
 
-	//app.use("public", express.static(__dirname + 'public'));
-	app.use(express.static(path.join(__dirname, 'public')));
-
+    app.use(express.static('public'));
 
 	// =====================================
 	// HOME PAGE (with login links) ========
