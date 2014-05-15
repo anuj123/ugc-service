3aa8f3583658a77dae285dea8b9f6d4787cb6a6b
Anuj Thapliyal <athapliyal@yp.com>
2014-05-15 11:13:03 -0700

    changing config to server

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-	'url' : 'mongodb://localhost/ugc_service' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://mobdev.np.wc1.yellowpages.com/ugc_service' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
